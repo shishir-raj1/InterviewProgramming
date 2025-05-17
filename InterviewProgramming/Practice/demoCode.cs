fb8269b2ea8f415319e22e79bfa2950b88039959
SHISHIR RAJ <shishirraj1997@gmail.com>
2025-05-17 10:15:34 +0530

    reverse

---
+++ InterviewProgramming/Practice/demoCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProgramming.Practice
+{
+    internal class demoCode
+    {
+
+        public static void reverseM()
+        {
+            string str = "SHISHIR";
+            string rev = "";
+
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                rev += str[i];
+            }
+            Console.WriteLine(rev);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            reverseM();        
+        }
+    }
+}
