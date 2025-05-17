f3caa1f63754e48e29d0935adcfe2ebb5afc6e44
SHISHIR RAJ <shishirraj1997@gmail.com>
2025-05-17 16:17:28 +0530

    car class

---
+++ InterviewProgramming/oopsProgramming/car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProgramming.oopsProgramming
+{
+    internal class car
+    {
+        //member variable, private hides the variable from other classes
+        private string _model = "";
+        private string _brand = "";
+        private bool _isLuxury;
+
+        //property with lambda expression
+        public string Model { get => _model; set => _model = value; }
+        public string Brand
+        {
+            get
+            {
+                if (_isLuxury)
+                {
+                    return _brand + " - Luxury Edition";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _brand = value;
+                }
+                else
+                {
+                    _brand = value;
+                }
+
+            }
+
+        }
+
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+
+        //custom constructor
+
+        public car(string model,string brand, bool isLuxury)
+        {
+            Model = model;
+            Brand = brand;
+            IsLuxury = isLuxury;
+        }
+
+    }
+}
